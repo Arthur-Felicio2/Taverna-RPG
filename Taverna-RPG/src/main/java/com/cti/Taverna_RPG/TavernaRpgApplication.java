f6beef5b9839782b74e8fd3b5501d3b74fcbcc8b
rbarb <rbarb@FELICIO>
2026-09-22 17:17:58 -0300

    first commit

---
+++ Taverna-RPG/src/main/java/com/cti/Taverna_RPG/TavernaRpgApplication.java
@@ -0,0 +1,13 @@
+package com.cti.Taverna_RPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernaRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavernaRpgApplication.class, args);
+	}
+
+}
---
+++ Taverna-RPG/src/test/java/com/cti/Taverna_RPG/TavernaRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cti.Taverna_RPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernaRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
